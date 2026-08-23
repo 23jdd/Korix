cb81339d2717220a380a50aa3bb9f75fef1becc6
long <ab1234_1123@qq.com>
2026-08-23 23:03:53 +0800

    docs(example): add runnable basic search

---
+++ examples/basic/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	koris "github.com/23jdd/Koris"
+	"github.com/23jdd/Koris/document"
+	"github.com/23jdd/Koris/highlight"
+	"github.com/23jdd/Koris/query"
+)
+
+func main() {
+	idx, err := koris.New()
+	if err != nil {
+		panic(err)
+	}
+	defer idx.Close()
+
+	_, err = idx.AddBatch([]document.Document{
+		{ID: "go", Fields: map[string]string{"title": "Learning Go", "content": "Go is fast", "category": "book"}},
+		{ID: "systems", Fields: map[string]string{"title": "Systems", "content": "A distributed system written in Golang", "category": "article"}},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	hits, err := idx.Search(query.MatchQuery{Field: "content", Text: "distributed golang", Operator: query.AND}, 10)
+	if err != nil {
+		panic(err)
+	}
+	for _, hit := range hits {
+		fragments, err := idx.Highlight(hit.DocID, "content", []string{"distributed", "golang"}, highlight.DefaultOptions())
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("id=%s score=%.3f snippet=%s\n", hit.ID, hit.Score, fragments[0].Text)
+	}
+}
